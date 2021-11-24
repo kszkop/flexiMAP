658ebb3136453d0e84581041ca2fd7189c71fc96
kszkop <krzysztof.szkop@gmail.com>
2021-11-24 13:10:44 +0000

    KS

--- R/flexiMAP_counting.R
+++ R/flexiMAP_counting.R
@@ -1,4 +1,4 @@
-flexiMAP_counting <- function(listBams, UTRannotation, nthreads)
+flexiMAP_counting <- function(listBams, UTRannotation, nthreads, pairedEnd=FALSE)
 {
   #Initate output
   UTRcounts <- list()
@@ -8,9 +8,13 @@ flexiMAP_counting <- function(listBams, UTRannotation, nthreads)
   featureCounts_annot_short <- UTRannotation[[2]]
 
   #Counting
-  long_count <- Rsubread::featureCounts(listBams,annot.ext=featureCounts_annot_long,allowMultiOverlap = FALSE, ignoreDup=TRUE,useMetaFeatures=FALSE,nthreads=nthreads)
-  short_count <- Rsubread::featureCounts(listBams,annot.ext=featureCounts_annot_short,allowMultiOverlap=TRUE, ignoreDup=TRUE,useMetaFeatures=FALSE,nthreads=nthreads)
-
+  if(!isTRUE(pairedEnd){
+    long_count <- Rsubread::featureCounts(listBams,annot.ext=featureCounts_annot_long,allowMultiOverlap = FALSE, ignoreDup=TRUE,useMetaFeatures=FALSE,nthreads=nthreads, isPairedEnd=FALSE)
+    short_count <- Rsubread::featureCounts(listBams,annot.ext=featureCounts_annot_short,allowMultiOverlap=TRUE, ignoreDup=TRUE,useMetaFeatures=FALSE,nthreads=nthreadsis, isPairedEnd=FALSE)
+  } else {
+    long_count <- Rsubread::featureCounts(listBams,annot.ext=featureCounts_annot_long,allowMultiOverlap = FALSE, ignoreDup=TRUE,useMetaFeatures=FALSE,nthreads=nthreads, isPairedEnd=TRUE, countReadPairs = TRUE, requireBothEndsMapped = FALSE)
+    short_count <- Rsubread::featureCounts(listBams,annot.ext=featureCounts_annot_short,allowMultiOverlap=TRUE, ignoreDup=TRUE,useMetaFeatures=FALSE,nthreads=nthreads,isPairedEnd=TRUE, countReadPairs = TRUE, requireBothEndsMapped = FALSE)
+  }
   #Output
   UTRcounts[[1]] <- as.data.frame(long_count$counts)
   UTRcounts[[2]] <- as.data.frame(short_count$counts)
